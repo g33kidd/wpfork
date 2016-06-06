e91f479847ba51e7f9156f2f27f114d95c83635c
Joshua Kidd <kidd.josh.343@gmail.com>
2016-06-06 12:09:11 -0600

    Added some more bootstrap code

--- core/public/index.php
+++ core/public/index.php
@@ -1,5 +1,18 @@
 <?php
 
 // Register the autoloader
-
 require __DIR__.'/../boot/autoload.php';
+
+// Create the app
+$app = require_once __DIR__.'/../boot/app.php';
+
+// Run the application
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
